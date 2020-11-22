964e9c1ab9415b1296fffb51565c684d95e52fc0
wjankat <74848111+wjankat@users.noreply.github.com>
2020-11-22 08:13:20 +0300

    Update config.php

--- opt/panel/data/config.php
+++ opt/panel/data/config.php
@@ -11,7 +11,7 @@ define('KT_UPDATE_URL_MC','http://s3.amazonaws.com/MinecraftDownload/launcher/mi
 define('KT_UPDATE_URL_CB','http://dl.bukkit.org/latest-rb/craftbukkit.jar');
 
 // Screen commands (these should never be modified)
-define('KT_SCREEN_CMD_START','screen -dmS %s java -Xms%sM -Xmx%sM -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar craftbukkit.jar nogui');
+define('KT_SCREEN_CMD_START','screen -dmS %s java -Xms64M -Xmx3048M -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=45 -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -jar craftbukkit.jar nogui');
 define('KT_SCREEN_CMD_EXEC','screen -S %s -p 0 -X stuff "%s$(printf \\\\r)"');
 define('KT_SCREEN_CMD_KILL','screen -X -S %s quit');
 define('KT_SCREEN_CMD_KILLALL','killall screen');
@@ -19,4 +19,4 @@ define('KT_SCREEN_CMD_KILLALL_USER','for session in $(screen -ls | /bin/grep -o 
 
 // ngrok commands
 define('NGROK_ID','mc.scid-');
-define('NGROK_START','screen -dmS %s ./ngrok tcp -config=%s/ngrok.yml %s');
+define('NGROK_START','screen -dmS %s ./ngrok tcp --region eu -config=%s/ngrok.yml %s');
